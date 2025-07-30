82fb98ee576d9975950e5992bc135949ca9a36f8
Kamil <kosiorek.kamil90@gmail.com>
2025-07-30 12:18:09 +0200

    fixed progress spinner in article component

--- my-website-angular/src/app/views/home-view/article/article.component.ts
+++ my-website-angular/src/app/views/home-view/article/article.component.ts
@@ -1,9 +1,17 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 import { ArticleStore } from '../../../../store/article.store';
 import { ArticleService } from '../services/article.service';
 import { NotificationService } from '../../../shared/services/notification.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-article',
@@ -14,6 +22,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class ArticleComponent implements OnInit {
   slug = input.required<string>();
   readonly articleStore = inject(ArticleStore);
+  articleLoading = signal(false);
 
   constructor(
     private articleService: ArticleService,
@@ -26,10 +35,14 @@ export class ArticleComponent implements OnInit {
 
   ngOnInit(): void {
     if (!this.article()?.markdownContent) {
-      this.articleService.getArticle(this.slug()).subscribe({
-        next: (res) => this.articleStore.updateArticle(res),
-        error: (error) => this.notificationService.showError(error),
-      });
+      this.articleLoading.set(true);
+      this.articleService
+        .getArticle(this.slug())
+        .pipe(finalize(() => this.articleLoading.set(false)))
+        .subscribe({
+          next: (res) => this.articleStore.updateArticle(res),
+          error: (error) => this.notificationService.showError(error),
+        });
     }
   }
 }
