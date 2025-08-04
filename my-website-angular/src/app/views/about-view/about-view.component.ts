40d1ff0d834fdf286d36a0dcb21f6aeab0cede7b
Kamil <kosiorek.kamil90@gmail.com>
2025-08-04 10:29:48 +0200

    optimized authentication method; changed outdated text in about view component

--- my-website-angular/src/app/views/about-view/about-view.component.ts
+++ my-website-angular/src/app/views/about-view/about-view.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-view',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about-view.component.html',
   styleUrl: './about-view.component.scss',
 })
--- website-backend/src/main/java/com/kosiorek/website/modules/auth/service/AuthenticationService.java
+++ website-backend/src/main/java/com/kosiorek/website/modules/auth/service/AuthenticationService.java
@@ -12,6 +12,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -47,13 +48,13 @@ public class AuthenticationService {
 
     @Transactional
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
+        Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getUsername(),
                         request.getPassword()
                 )
         );
-        User user = userService.findByUsername(request.getUsername());
+        User user = (User) authentication.getPrincipal();
         String jwt = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwt)
